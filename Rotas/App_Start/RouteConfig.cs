80d915d88a8de164c3e50712fca74da4d7baf4a8
Vitor Hugo <vitorhugooliveira64@gmail.com>
2018-05-23 23:46:57 -0300

    Parou na parte que fala sobre Bundle

--- Rotas/App_Start/RouteConfig.cs
+++ Rotas/App_Start/RouteConfig.cs
@@ -13,6 +13,24 @@ namespace Rotas
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.MapRoute( // Rota Nova
+                name:"Todas As Noticias", // Nome Definido Para A Url
+                url:"noticias/", // forma com a rota sera chamada na url //aconselhado colocar a barra depois do nome, caso tenha algo depois não da erro
+                defaults: new { controller = "Home", action = "TodasAsNoticias"} // aqui é informado o Model e a função que será atribuída a rota
+            );
+
+            routes.MapRoute( // Rota Nova
+                name: "Categoria Especifica", // Nome Definido Para A Url
+                url: "noticias/{categoria}", // quando a action for chamada será necessário passar um parametro para ela, então é colocado entre chaves o parametro da url
+                defaults: new { controller = "Home", action = "MostraCategoria" } // aqui é informado o Model e a função que será atribuída a rota
+            );
+
+            routes.MapRoute( // Rota Nova
+                name: "Mostra Noticia", // Nome Definido Para A Url
+                url: "noticias/{categoria}/{titulo}-{noticiaId}", // quando a action for chamada será necessário passar um parametro para ela, então é colocado entre chaves o parametro da url
+                defaults: new { controller = "Home", action = "MostraNoticia" } // aqui é informado o Model e a função que será atribuída a rota
+            );
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
--- Rotas/Controllers/HomeController.cs
+++ Rotas/Controllers/HomeController.cs
@@ -9,7 +9,7 @@ namespace Rotas.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly IEnumerable<Noticia> todasAsNoticias;
+        private readonly IEnumerable<Noticia> todasAsNoticias; // essa variável será passada pela View Tipada
 
         public HomeController()
         {
@@ -21,7 +21,7 @@ namespace Rotas.Controllers
             var ultimasNoticias = todasAsNoticias.Take(3); // pega os tres primeiros valores da variavel todasAsNoticias
             var todasAsCategorias = todasAsNoticias.Select(x => x.Categoria).Distinct(); // pega todas as categorias que são diferente
             ViewBag.todasAsCategorias = todasAsCategorias;
-            return View(ultimasNoticias);
+            return View(ultimasNoticias); // aqui a variável é passada pela view tipada
         }
 
         public ActionResult About()
@@ -36,6 +36,24 @@ namespace Rotas.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public ActionResult TodasAsNoticias()
+        {
+            return View(todasAsNoticias); // aqui a variável é passada pela view tipada
+        }
+
+        public ActionResult MostraNoticia(int noticiaId, string titulo, string categoria) // aqui será recebidos esses paramentros para definir o que será mostrado
+        {
+            return View(todasAsNoticias.FirstOrDefault(x => x.NoticiaId == noticiaId)); // nessa parte será passado o id da noticia que foi clicado, e isso fara um  filtro para mostrar somente o id da noticia que está sendo informado
+        }
+
+        public ActionResult MostraCategoria(string categoria)
+        {
+            // esssa próxima linha verifica se a categoria informada é igual a alguma categoria do banco, se for ela vai listar na variável
+            var categoriaEspecifica = todasAsNoticias.Where(x => x.Categoria.ToLower() == categoria.ToLower()).ToList();
+            ViewBag.Categoria = categoria; // viewbag vai receber o categoria especifica
+            return View(categoriaEspecifica); // iforma a categoria especifica
         }
     }
 }
